7413ca063efe77b7ce2e8069defff3f71030c7f0
thant-sin-aung <thantsin92@gmail.com>
2023-04-30 17:12:06 +0800

    WIP

--- src/components/ToiletDashboard/ToiletDashboard.jsx
+++ src/components/ToiletDashboard/ToiletDashboard.jsx
@@ -1,53 +1,77 @@
-import React, { useEffect,useState } from "react";
-import { useHistory, useLocation,useSearchParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useHistory, useLocation, useSearchParams } from "react-router-dom";
 import './ToiletDashboard.css'; // Import the CSS file
-import { getMeasurements } from "../../api/urineAnalysis";
+import { getMeasurements, getRecommendedDiet } from "../../api/urineAnalysis";
 
 const ToiletDashboardPage = () => {
   const history = useHistory();
   const location = useLocation();
   const { userName, loggedInUserObj } = location.state;
+  var phresult = "";
+  var tempresult = "";
+  var recom = "";
 
-  
+  useEffect(() => {
+    console.log("Inisde useEffect",phresult,tempresult,recom)
+  }, [phresult,tempresult,recom]); 
 
   const searchParams = new URLSearchParams(window.location.search);
   const deviceid = searchParams.get('deviceid');
   const personGuid = searchParams.get('guid');
 
-  const myInterval = setInterval(myTimer, 1000);
- console.log(myInterval);
+  const myInterval = setInterval(myTimer, 10000);
+  console.log(myInterval);
   async function myTimer() {
     const date = new Date();
     console.log(date.toLocaleTimeString());
-    let params={};
-      params['personGuid'] = personGuid;
-      params['deviceId'] = deviceid;
-      params['last'] = 1;
+    let params = {};
+    params['personGuid'] = personGuid;
+    params['deviceId'] = deviceid;
+    params['last'] = 1;
 
-      const body = { 
-        device_id: deviceid
-      }; 
-    const response  = await getMeasurements(body);  
-    console.log("this is get measurements " , response)
-    console.log(myInterval);
-    if(response[0] == 0 || response[1] == 0 ){
-      //check again
-      clearInterval(myInterval);
-    }else{
+    const body = {
+      device_id: deviceid
+    };
+    const response = await getMeasurements(body);
+    console.log("this is get measurements ", response)
+    console.log("this is response[0]",response[0][0]);
+
+    if (response[0][0] != 0.0 && response[0][1] != 0.0) {
+    console.log("this is after response ", response );
+      phresult = response[0][0].toString();
+      tempresult = response[0][1].toString();
+      
       //alert("PH is" + response[0] + ": Temp is" +  response[1])
       //call recommandation
-      
+      let response1 = await getRecommendedDiet(response[0][0]);
+      console.log(response1);
+      recom =response1;
+      clearInterval(myInterval);
+    } else {
+      //check again 
+      phresult = response[0][0].toString();
+      tempresult = response[0][1].toString();
+
+      let response1 = await getRecommendedDiet(response[0][0]);
+      console.log(response1);
+      recom =response1;
       clearInterval(myInterval);
     }
-    
+
   }
 
-  
+
 
 
 
   return (
-   <p>Waiting Result  {loggedInUserObj.userName}</p>
+    <div>
+      <p>Waiting Result  {loggedInUserObj.userName}</p>
+      
+      <p>PH is  {phresult}</p>
+      <p>Temperature is  {tempresult}</p>
+      <p>Recommendation is  {recom}</p>
+    </div>
   );
 };
 
