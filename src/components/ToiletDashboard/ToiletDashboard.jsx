e7a2fade429dd3fb925dd9bafb80e663b6bdf243
phyu <sow17809@gmail.com>
2023-06-17 10:39:28 +0800

    ..

--- src/components/ToiletDashboard/ToiletDashboard.jsx
+++ src/components/ToiletDashboard/ToiletDashboard.jsx
@@ -9,13 +9,13 @@ const ToiletDashboardPage = () => {
   const { userName, loggedInUserObj } = location.state;
   var phresult = "a";
   var tempresult = "b";
-  var recom = "c";
+  var recom = "No recommendation";
   const searchParams = new URLSearchParams(window.location.search);
   const deviceid = searchParams.get('deviceid');
   const personGuid = searchParams.get('guid');
 
   const myInterval = setInterval(myTimer, 10000);
-    console.log(myInterval); 
+  console.log(myInterval);
 
   async function myTimer() {
     const date = new Date();
@@ -29,29 +29,31 @@ const ToiletDashboardPage = () => {
       device_id: deviceid
     };
     const response = await getMeasurements(body);
-    console.log("this is get measurements ", response) 
+    console.log("this is get measurements ", response)
 
     if (response[0][0] != 0.0 && response[0][1] != 0.0) {
-    console.log("this is after response ", response );
+      console.log("this is after response ", response);
       phresult = response[0][0].toString();
       tempresult = response[0][1].toString();
-       
-      //call recommandation
-      let response1 = await getRecommendedDiet(response[0][0]);
-      console.log(response1);
-      recom =response1.recommended_items.toString();
-      let urineresult = "";
-      if (response[0][0] < 7){
-        urineresult = "Your urine is aciditic!";
-      }else{
-        urineresult = "Your urine is not aciditic!"; 
-      }
-      alert("PH is " + phresult + ": Temp is " +  tempresult + ": " + urineresult + " Recommandation foods to eat are " +  recom);
 
-      
+
+      let urineresult = "";
+      if (response[0][0] < 7) {
+        urineresult = "Your urine is acidic!";
+        //call recommandation
+        let response1 = await getRecommendedDiet(response[0][0]);
+        console.log(response1);
+        recom = response1.recommended_items.toString();
+        alert("PH is " + phresult + ": Temp is " + tempresult + ": " + urineresult + " Recommandation foods to eat are " + recom);
+      } else {
+        urineresult = "Your urine is not acidic!";
+        alert("PH is " + phresult + ": Temp is " + tempresult + ": " + urineresult);
+      }
+
+
       clearInterval(myInterval);
       window.close();
-    } else { 
+    } else {
       //check again  
     }
   }
